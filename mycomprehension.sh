3b7d67fe427790a71ccf65961fec9b5700be1f9d
engruju <ujunwandaline@gmail.com>
2024-03-22 20:39:52 +0000

    the assignment

---
+++ mycomprehension.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "my name is Mrs Ujunwa Ozouli, married to Engr Ikechukwu Ozouli"
+echo "i am 29years old and i am new to software Engineering"#!/becho "i live at no 3 Oxford street GRA Enugu"
+echo "i am currently working on my project"
+
+#ask a question on who is reading this 
+echo "who is this?"
+read name 
+echo "hello, $name! nice to meet you."
+echo "todays date is"
+date
+echo "this computer has this memory storage"
+memory= free -h
+echo "$memory"
