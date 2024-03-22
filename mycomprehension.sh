49f349a83b46e426722d5725ddd52ba5da1e3569
Tony Ike Ozouli <flawlez18@yahoo.co.uk>
2024-03-22 17:15:01 -0500

    changed memory command in mycomprehension.sh

--- mycomprehension.sh
+++ mycomprehension.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 echo "my name is Mrs Ujunwa Ozouli, married to Engr Ikechukwu Ozouli"
-echo "i am 29years old and i am new to software Engineering"#!/becho "i live at no 3 Oxford street GRA Enugu"
+echo "i am 29years old and i am new to software Engineering" 
+echo "i live at no 3 Oxford street GRA Enugu"
 echo "i am currently working on my project"
 
-#ask a question on who is reading this 
+# ask a question on who is reading this 
 echo "who is this?"
 read name 
 echo "hello, $name! nice to meet you."
 echo "todays date is"
 date
 echo "this computer has this memory storage"
-memory= free -h
+diskspace= top -l 1 | grep PhysMem
 echo "$memory"
